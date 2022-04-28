16095f0a937392ebfba715a85020e06c40d80059
Clayton Knapp <csjknapp@gmail.com>
2022-04-27 19:39:44 -0500

    added data in correct shape, tests passing with mocked data

--- src/views/List.test.jsx
+++ src/views/List.test.jsx
@@ -37,27 +37,13 @@ const data = {
   ],
 };
 
-const data2 = [
-  {
-    _score: 22535.4,
-    artist_title: 'Alma Thomas',
-    id: 129884,
-    image_id: 'e966799b-97ee-1cc6-bd2f-a94b4b8bb8f9',
-    title: 'Starry Night and the Astronauts',
-  },
-  {
-    _score: 834.6445,
-    artist_title: 'Joan Mitchell',
-    id: 86385,
-    image_id: 'f7f9615d-2c2b-6b23-47b2-cd6cdc846504',
-    title: 'City Landscape',
-  },
-];
-
 const server = setupServer(
-  rest.get(
-    'https://api.artic.edu/api/v1/artworks/search?fields=id,title,artist_title,image_id&limit=20',
-    (req, res, ctx) => res(ctx.json(data2))
+  // rest.get(
+  //   'https://api.artic.edu/api/v1/artworks/search?fields=id,title,artist_title,image_id&limit=20',
+  //   (req, res, ctx) => res(ctx.json(data1))
+  // )
+  rest.get('https://api.artic.edu/api/v1/artworks/search', (req, res, ctx) =>
+    res(ctx.json(data))
   )
 );
 
