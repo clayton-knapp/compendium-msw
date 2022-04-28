19bdf12396168a37c6f3743e992ca4dafaaad408
Clayton Knapp <csjknapp@gmail.com>
2022-04-27 19:33:03 -0500

    added beforeAll afterAll

--- src/views/List.test.jsx
+++ src/views/List.test.jsx
@@ -10,7 +10,62 @@ import { MemoryRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import List from './List';
 
+const data = {
+  preference: null,
+  pagination: {
+    total: 116021,
+    limit: 20,
+    offset: 0,
+    total_pages: 5802,
+    current_page: 1,
+  },
+  data: [
+    {
+      _score: 22535.4,
+      artist_title: 'Alma Thomas',
+      id: 129884,
+      image_id: 'e966799b-97ee-1cc6-bd2f-a94b4b8bb8f9',
+      title: 'Starry Night and the Astronauts',
+    },
+    {
+      _score: 834.6445,
+      artist_title: 'Joan Mitchell',
+      id: 86385,
+      image_id: 'f7f9615d-2c2b-6b23-47b2-cd6cdc846504',
+      title: 'City Landscape',
+    },
+  ],
+};
+
+const data2 = [
+  {
+    _score: 22535.4,
+    artist_title: 'Alma Thomas',
+    id: 129884,
+    image_id: 'e966799b-97ee-1cc6-bd2f-a94b4b8bb8f9',
+    title: 'Starry Night and the Astronauts',
+  },
+  {
+    _score: 834.6445,
+    artist_title: 'Joan Mitchell',
+    id: 86385,
+    image_id: 'f7f9615d-2c2b-6b23-47b2-cd6cdc846504',
+    title: 'City Landscape',
+  },
+];
+
+const server = setupServer(
+  rest.get(
+    'https://api.artic.edu/api/v1/artworks/search?fields=id,title,artist_title,image_id&limit=20',
+    (req, res, ctx) => res(ctx.json(data2))
+  )
+);
+
 describe('Component and Behavioral Tests', () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
   it('Should test the component for loading state and one pokemon', async () => {
     render(<List />);
 
